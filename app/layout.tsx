fbc1114ed6d4b6693c181d4cf3979957018998b5
IhorMtr <igormotornyyy@gmail.com>
2025-10-07 20:19:10 +0300

    initialized project

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  variable: '--font-family',
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
